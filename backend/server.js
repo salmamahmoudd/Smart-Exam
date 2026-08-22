8fc31b4d575ad214ff93de2ffd682dfd173364fa
Salma Mahmoud <salmamahmoudd.com>
2026-08-22 18:58:57 +0300

    Fix Vercel CORS

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
+    origin: function (origin, callback) {
       if (!origin) {
         return callback(null, true);
       }

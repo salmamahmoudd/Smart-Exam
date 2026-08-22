db3b98a8bd3f4b31aaa8ed0b94558781ab280aa1
Salma Mahmoud <salmamahmoudd.com>
2026-08-22 18:51:43 +0300

    Fix Runsite port configuration

--- backend/server.js
+++ backend/server.js
@@ -71,10 +71,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(
-    `Server running on port ${PORT}`
-  );
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
